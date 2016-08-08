80a1ca2c72b5e88b96cd674b3ae3868ef0e30e4a
thilakas <thilikas29@gmail.com>
2016-08-08 13:38:24 +0530

    Add files via upload

---
+++ sumno.java
@@ -0,0 +1,15 @@
+package logic;
+
+public class sumno {
+
+	public static void main(String args[])
+	{
+		int sum = 0;
+		int n=10;
+		for(int i = 1;i <= n;i++)
+		{
+			sum = sum + i;
+		}
+		System.out.println("The Sum Of "+n+" Numbers are:" + sum);
+	}
+}
